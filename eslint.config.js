ba813926fc2a3afb6aefd66f291142d93eb3cdeb
cbMan0 <cbman0@local>
2026-03-04 19:42:30 -0500

    chore(tools): add ESLint config and update dependencies

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from "globals";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-console": "off",
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
